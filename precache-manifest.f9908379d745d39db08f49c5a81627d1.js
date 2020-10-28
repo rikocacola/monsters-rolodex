ddc7f01fd903abd30020b55154161109fa3c4a7b
Riko Chair Nugroho <riko.chair@gmail.com>
2020-10-28 14:21:59 +0700

    Updates

---
+++ precache-manifest.f9908379d745d39db08f49c5a81627d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c6ae9e10739be376ea887f2c0b2992",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89e53aef061a2b673413",
+    "url": "/monsters-rolodex/static/css/main.e51f77b8.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e53aef061a2b673413",
+    "url": "/monsters-rolodex/static/js/main.bbba4083.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
